8c7502d1241494a6a59fe78e86b0ebcccc795771
arnav <arnav@mapmyindia.com>
2019-07-19 14:43:40 +0530

    Updates

---
+++ precache-manifest.02a05eaedaa7d8a4176cbb92ec47f167.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e697c7142939b195a3b54599cec278",
+    "url": "/papastuff/index.html"
+  },
+  {
+    "revision": "2c9b0d39a1774a1f6d02",
+    "url": "/papastuff/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/papastuff/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "2c9b0d39a1774a1f6d02",
+    "url": "/papastuff/static/js/main.8376e7d5.chunk.js"
+  },
+  {
+    "revision": "863db381155f2427bc97",
+    "url": "/papastuff/static/js/runtime~main.b94a34f8.js"
+  }
+]);
